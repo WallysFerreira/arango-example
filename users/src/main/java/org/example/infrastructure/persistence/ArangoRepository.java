a932e988ea0dca5360b034e3767354d5552c78dc
Wallys Ferreira <wallysfsilva@gmail.com>
2024-10-24 09:58:36 -0300

    change connection check

--- users/src/main/java/org/example/infrastructure/persistence/ArangoRepository.java
+++ users/src/main/java/org/example/infrastructure/persistence/ArangoRepository.java
@@ -14,6 +14,6 @@ public class ArangoRepository {
     }
 
     public void testConnection() {
-        System.out.println(arangoDB.getServerId());
+        System.out.println(arangoDB.getDatabases().iterator().next());
     }
 }
